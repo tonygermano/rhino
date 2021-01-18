8f523e58ee35c3f2450fda4dc3fa4a48706883bb
Roland Praml <roland.praml@foconis.de>
2021-01-18 11:32:17 +0100

    FIX: merge errors

--- src/org/mozilla/javascript/NativeJavaList.java
+++ src/org/mozilla/javascript/NativeJavaList.java
@@ -10,7 +10,7 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NativeJavaList extends NativeJavaObject {
+public class NativeJavaList extends NativeJavaObject implements ArrayScriptable {
 
     private static final long serialVersionUID = 6403865639690547921L;
 
@@ -62,6 +62,9 @@ public class NativeJavaList extends NativeJavaObject {
         if (isWithValidIndex(index)) {
             Context cx = Context.getContext();
             Object obj = list.get(index);
+            if (obj == null) {
+                return null;
+            }
             return cx.getWrapFactory().wrap(cx, this, obj, obj.getClass());
         }
         return Undefined.instance;
@@ -96,6 +99,11 @@ public class NativeJavaList extends NativeJavaObject {
         }
     }
 
+    @Override
+    public long getLength() {
+        return list.size();
+    }
+    
     @Override
     public Object[] getIds() {
         List<?> list = (List<?>) javaObject;
